da4ed1ce16b2f69d8cd61e5e592c08d1ebf424dc
tianrking <tian.r.king@gmail.com>
2023-12-18 12:11:26 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Button, TextInput, Alert } from 'react-native';
+import Modal from 'react-native-modal';
 import axios from 'axios';
 import * as Location from 'expo-location';
 import { Gyroscope, Accelerometer, Magnetometer, Barometer } from 'expo-sensors';
@@ -10,38 +11,59 @@ export default function App() {
   const [accelData, setAccelData] = useState({});
   const [magnetometerData, setMagnetometerData] = useState({});
   const [barometerData, setBarometerData] = useState({});
-
-  const SERVER_URL = 'http://yourserver.com:port'; // 替换为你的服务器地址和端口
+  const [serverUrl, setServerUrl] = useState('http://yourserver.com:port');
+  const [isModalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    async function subscribeToSensors() {
+    let locationSubscription;
+  
+    const subscribeToSensors = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status === 'granted') {
-        Location.watchPositionAsync({
-          accuracy: Location.Accuracy.BestForNavigation,
-          timeInterval: 1000
+        locationSubscription = await Location.watchPositionAsync({
+          accuracy: Location.Accuracy.High,
+          timeInterval: 500, // 每0.5秒更新一次
+          distanceInterval: 1, // 或者每移动1米更新一次
         }, setLocation);
       }
-
+  
       Gyroscope.setUpdateInterval(1000);
+      const gyroSubscription = Gyroscope.addListener(data => setGyroData(data));
+  
       Accelerometer.setUpdateInterval(1000);
+      const accelSubscription = Accelerometer.addListener(data => setAccelData(data));
+  
       Magnetometer.setUpdateInterval(1000);
+      const magnetometerSubscription = Magnetometer.addListener(data => setMagnetometerData(data));
+  
       Barometer.setUpdateInterval(1000);
-
-      Gyroscope.addListener(data => setGyroData(data));
-      Accelerometer.addListener(data => setAccelData(data));
-      Magnetometer.addListener(data => setMagnetometerData(data));
-      Barometer.addListener(data => setBarometerData(data));
-    }
-
+      const barometerSubscription = Barometer.addListener(data => setBarometerData(data));
+  
+      return () => {
+        gyroSubscription.remove();
+        accelSubscription.remove();
+        magnetometerSubscription.remove();
+        barometerSubscription.remove();
+        if (locationSubscription) {
+          locationSubscription.remove(); // 确保取消位置订阅
+        }
+      };
+    };
+  
     subscribeToSensors();
-
+  
     const dataSendInterval = setInterval(() => {
       sendData();
     }, 2000);
-
-    return () => clearInterval(dataSendInterval);
-  }, []);
+  
+    return () => {
+      clearInterval(dataSendInterval);
+      if (locationSubscription) {
+        locationSubscription.remove(); // 清理位置订阅
+      }
+    };
+  }, [serverUrl]); // 依赖项列表中添加serverUrl
+  
 
   const sendData = async () => {
     const payload = {
@@ -53,11 +75,30 @@ export default function App() {
     };
 
     try {
-      await axios.post(`${SERVER_URL}/send-data`, payload);
+      await axios.post(`${serverUrl}/send-data`, payload);
       console.log('Data sent successfully');
     } catch (error) {
       console.error('Error sending data:', error);
     }
+  };
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const handleServerUrlChange = (text) => {
+    setServerUrl(text);
+  };
+
+  const renderSensorData = (data, sensorName) => {
+    return (
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>{sensorName} Data:</Text>
+        {data && Object.entries(data).map(([key, value]) => (
+          <Text key={key} style={styles.dataText}>{`${key}: ${value.toFixed(3)}`}</Text>
+        ))}
+      </View>
+    );
   };
 
   const renderLocationData = () => {
@@ -77,25 +118,28 @@ export default function App() {
     return <Text style={styles.infoText}>Waiting for location data...</Text>;
   };
 
-  const renderSensorData = (data, sensorName) => {
-    return (
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>{sensorName} Data:</Text>
-        {data && Object.entries(data).map(([key, value]) => (
-          <Text key={key} style={styles.dataText}>{`${key}: ${value.toFixed(3)}`}</Text>
-        ))}
-      </View>
-    );
-  };
-
   return (
-    <ScrollView style={styles.container}>
-      {renderLocationData()}
-      {renderSensorData(gyroData, 'Gyroscope')}
-      {renderSensorData(accelData, 'Accelerometer')}
-      {renderSensorData(magnetometerData, 'Magnetometer')}
-      {renderSensorData(barometerData, 'Barometer')}
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView>
+        {renderLocationData()}
+        {renderSensorData(gyroData, 'Gyroscope')}
+        {renderSensorData(accelData, 'Accelerometer')}
+        {renderSensorData(magnetometerData, 'Magnetometer')}
+        {renderSensorData(barometerData, 'Barometer')}
+      </ScrollView>
+      <Button title="Set Server Address" onPress={toggleModal} />
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.modalContent}>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleServerUrlChange}
+            value={serverUrl}
+            placeholder="Enter Server URL"
+          />
+          <Button title="Save" onPress={toggleModal} />
+        </View>
+      </Modal>
+    </View>
   );
 }
 
@@ -103,6 +147,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 50,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 200,
+    marginBottom: 10,
   },
   section: {
     margin: 15,
