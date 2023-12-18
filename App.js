9e01f3c1b57b31acd033598b3024f7a44d8f46af
tianrking <tian.r.king@gmail.com>
2023-12-18 11:30:55 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,219 +1,125 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Button, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import axios from 'axios';
 import * as Location from 'expo-location';
-import { Gyroscope, Accelerometer, Barometer, Magnetometer } from 'expo-sensors';
+import { Gyroscope, Accelerometer, Magnetometer, Barometer } from 'expo-sensors';
 
 export default function App() {
   const [location, setLocation] = useState(null);
-  const [locationError, setLocationError] = useState(false);
-  const [gyroData, setGyroData] = useState(null);
-  const [accelData, setAccelData] = useState(null);
-  const [barometerData, setBarometerData] = useState(null);
-  const [magnetometerData, setMagnetometerData] = useState(null);
+  const [gyroData, setGyroData] = useState({});
+  const [accelData, setAccelData] = useState({});
+  const [magnetometerData, setMagnetometerData] = useState({});
+  const [barometerData, setBarometerData] = useState({});
+
+  const SERVER_URL = 'http://yourserver.com:port'; // 替换为你的服务器地址和端口
 
   useEffect(() => {
-    Gyroscope.setUpdateInterval(1000);
-    Accelerometer.setUpdateInterval(1000);
-    Barometer.setUpdateInterval(1000);
-    Magnetometer.setUpdateInterval(1000);
+    async function subscribeToSensors() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === 'granted') {
+        Location.watchPositionAsync({
+          accuracy: Location.Accuracy.BestForNavigation,
+          timeInterval: 1000
+        }, setLocation);
+      }
 
-    const gyroSubscription = Gyroscope.addListener(gyro => {
-      setGyroData(gyro);
-    });
-    const accelSubscription = Accelerometer.addListener(accel => {
-      setAccelData(accel);
-    });
-    const barometerSubscription = Barometer.addListener(data => {
-      setBarometerData(data);
-    });
-    const magnetometerSubscription = Magnetometer.addListener(data => {
-      setMagnetometerData(data);
-    });
+      Gyroscope.setUpdateInterval(1000);
+      Accelerometer.setUpdateInterval(1000);
+      Magnetometer.setUpdateInterval(1000);
+      Barometer.setUpdateInterval(1000);
 
-    const locationInterval = setInterval(() => {
-      getLocation();
-    }, 2000); // 每 200 ms更新一次位置
+      Gyroscope.addListener(data => setGyroData(data));
+      Accelerometer.addListener(data => setAccelData(data));
+      Magnetometer.addListener(data => setMagnetometerData(data));
+      Barometer.addListener(data => setBarometerData(data));
+    }
 
-    return () => {
-      gyroSubscription.remove();
-      accelSubscription.remove();
-      barometerSubscription.remove();
-      magnetometerSubscription.remove();
-      clearInterval(locationInterval); // 清理定时器
-    };
+    subscribeToSensors();
+
+    const dataSendInterval = setInterval(() => {
+      sendData();
+    }, 2000);
+
+    return () => clearInterval(dataSendInterval);
   }, []);
 
-  // const getLocation = async () => {
-  //   let { status } = await Location.requestForegroundPermissionsAsync();
-  //   if (status !== 'granted') {
-  //     setLocationError(true);
-  //     return;
-  //   }
+  const sendData = async () => {
+    const payload = {
+      location,
+      gyroData,
+      accelData,
+      magnetometerData,
+      barometerData
+    };
 
-  //   let location = await Location.getCurrentPositionAsync({});
-  //   setLocation(location);
-  //   setLocationError(false);
-  // };
-  // const getLocation = async () => {
-  //   const { status: existingStatus } = await Location.getForegroundPermissionsAsync();
-  //   let finalStatus = existingStatus;
-  
-  //   if (existingStatus !== 'granted') {
-  //     const { status } = await Location.requestForegroundPermissionsAsync();
-  //     finalStatus = status;
-  //   }
-  
-  //   if (finalStatus !== 'granted') {
-  //     setLocationError(true);
-  //     return;
-  //   }
-  
-  //   let location = await Location.getCurrentPositionAsync({});
-  //   setLocation(location);
-  //   setLocationError(false);
-  // };
-  const getLocation = async () => {
-    let { status } = await Location.getForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      let response = await Location.requestForegroundPermissionsAsync();
-      if (response.status !== 'granted') {
-        setLocationError(true);
-        return;
-      }
+    try {
+      await axios.post(`${SERVER_URL}/send-data`, payload);
+      console.log('Data sent successfully');
+    } catch (error) {
+      console.error('Error sending data:', error);
     }
-  
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
-    setLocationError(false);
   };
-  
-  
 
-  const renderLocationDetails = () => {
-    if (locationError) {
-      return <Text style={styles.errorText}>Permission to access location was denied</Text>;
-    }
-
+  const renderLocationData = () => {
     if (location) {
-      const { coords, timestamp } = location;
       return (
         <View style={styles.section}>
-          <Text style={styles.sectionTitle}>GPS Location:</Text>
-          {Object.entries(coords).map(([key, value]) => (
-            <Text key={key} style={styles.dataText}>
-              {`${key}: ${value}`}
-            </Text>
-          ))}
-          <Text style={styles.dataText}>Timestamp: {timestamp}</Text>
-          <Text style={styles.dataText}>Altitude: {coords.altitude} meters</Text>
+          <Text style={styles.sectionTitle}>Location Data:</Text>
+          <Text style={styles.dataText}>Latitude: {location.coords.latitude}</Text>
+          <Text style={styles.dataText}>Longitude: {location.coords.longitude}</Text>
+          <Text style={styles.dataText}>Altitude: {location.coords.altitude} meters</Text>
+          <Text style={styles.dataText}>Speed: {location.coords.speed} m/s</Text>
+          <Text style={styles.dataText}>Heading: {location.coords.heading} degrees</Text>
+          <Text style={styles.dataText}>Accuracy: {location.coords.accuracy} meters</Text>
         </View>
       );
     }
-
-    return <Text style={styles.infoText}>Waiting for GPS location...</Text>;
+    return <Text style={styles.infoText}>Waiting for location data...</Text>;
   };
 
   const renderSensorData = (data, sensorName) => {
-    if (data) {
-      return (
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>{sensorName} Data:</Text>
-          {Object.entries(data).map(([key, value]) => (
-            <Text key={key} style={styles.dataText}>
-              {`${key}: ${value.toFixed(3)}`}
-            </Text>
-          ))}
-        </View>
-      );
-    }
-    return <Text style={styles.infoText}>Waiting for {sensorName} data...</Text>;
-  };
-
-  const getAltitudeFromPressure = (pressure) => {
-    return 44330.0 * (1.0 - Math.pow(pressure / 1013.25, 0.1903));
-  };
-
-  const renderBarometerData = () => {
-    if (barometerData) {
-      const estimatedAltitude = getAltitudeFromPressure(barometerData.pressure);
-      return (
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Barometer Data:</Text>
-          <Text style={styles.dataText}>Pressure: {barometerData.pressure} hPa</Text>
-          <Text style={styles.dataText}>Estimated Altitude: {estimatedAltitude.toFixed(2)} meters</Text>
-        </View>
-      );
-    }
-    return <Text style={styles.infoText}>Waiting for barometer data...</Text>;
-  };
-
-  const renderMagnetometerData = () => {
-    if (magnetometerData) {
-      return (
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Magnetometer Data:</Text>
-          <Text style={styles.dataText}>x: {magnetometerData.x.toFixed(3)}</Text>
-          <Text style={styles.dataText}>y: {magnetometerData.y.toFixed(3)}</Text>
-          <Text style={styles.dataText}>z: {magnetometerData.z.toFixed(3)}</Text>
-        </View>
-      );
-    }
-    return <Text style={styles.infoText}>Waiting for magnetometer data...</Text>;
+    return (
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>{sensorName} Data:</Text>
+        {data && Object.entries(data).map(([key, value]) => (
+          <Text key={key} style={styles.dataText}>{`${key}: ${value.toFixed(3)}`}</Text>
+        ))}
+      </View>
+    );
   };
 
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        {renderLocationDetails()}
-        {renderSensorData(gyroData, 'Gyroscope')}
-        {renderSensorData(accelData, 'Accelerometer')}
-        {renderBarometerData()}
-        {renderMagnetometerData()}
-      </ScrollView>
-      <View style={styles.buttonContainer}>
-        <Button title="Get GPS Location" onPress={getLocation} />
-      </View>
-    </View>
+    <ScrollView style={styles.container}>
+      {renderLocationData()}
+      {renderSensorData(gyroData, 'Gyroscope')}
+      {renderSensorData(accelData, 'Accelerometer')}
+      {renderSensorData(magnetometerData, 'Magnetometer')}
+      {renderSensorData(barometerData, 'Barometer')}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    backgroundColor: '#f5f5f5',
-  },
-  scrollView: {
-    marginVertical: 20,
-    marginTop: 50,  // 增加顶部外边距
+    marginTop: 50,
   },
   section: {
-    marginBottom: 15,
-    paddingHorizontal: 20,
+    margin: 15,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
   },
   sectionTitle: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 5,
   },
   dataText: {
-    fontSize: 14,
+    fontSize: 16,
+    marginVertical: 2,
   },
   infoText: {
-    fontSize: 14,
+    fontSize: 16,
     fontStyle: 'italic',
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-  errorText: {
-    fontSize: 14,
-    color: 'red',
-    fontStyle: 'italic',
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-  buttonContainer: {
-    paddingBottom: 20,
-    paddingHorizontal: 20,
   },
 });
